22fef29aeb604df51b9b7feca9600a29e2fe39a7
kvanee <kelly.vanee@gmail.com>
2013-03-01 08:37:13 -0700

    Removed tests for publish.

--- ImportEngine/ImportPlayersSpecs.cs
+++ ImportEngine/ImportPlayersSpecs.cs
@@ -7,20 +7,20 @@ namespace ImportEngine
 {
     public class ImportPlayersSpecs : SpecsFor<PlayerAdministration>
     {
-        [Test]
-        public void I_should_be_able_to_get_players_world_ranking()
-        {
-            //SUT.GetWorldRankings(new[] { new Player("Tiger Woods", TODO), });
-        }
-
-        [Test]
-        public void I_should_be_able_to_drop_and_create_db()
-        {
-            GolfPoolEntities.RecreateAndSeedDB();
-            var repo = new Repository(new GolfPoolEntities());
-            repo.Insert(new GolferGroup());
-            repo.Save();
-
-        }
+//        [Test]
+//        public void I_should_be_able_to_get_players_world_ranking()
+//        {
+//            //SUT.GetWorldRankings(new[] { new Player("Tiger Woods", TODO), });
+//        }
+//
+//        [Test]
+//        public void I_should_be_able_to_drop_and_create_db()
+//        {
+//            GolfPoolEntities.RecreateAndSeedDB();
+//            var repo = new Repository(new GolfPoolEntities());
+//            repo.Insert(new GolferGroup());
+//            repo.Save();
+//
+//        }
     }
 }
